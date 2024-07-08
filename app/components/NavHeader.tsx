0aa72fdb218992290656de55e9cc0f853c59b0c3
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-07-08 18:19:34 -0400

    feat: ✨razzle dazzle✨
    Adds an icon lib, classnames package, update base colors, adds header to layout, and colors cards based on event magnitude

---
+++ app/components/NavHeader.tsx
@@ -0,0 +1,18 @@
+'use client';
+import Link from 'next/link';
+
+const NavHeader = () => {
+  return (
+    <div className="flex h-[60px] bg-purple-heart-950 gap-4 px-4 py-4">
+      <div className="w-[25px] bg-gray-500 " />
+      <Link className="text-white" href="/">
+        Earthquake Tracker
+      </Link>
+      <Link className="text-white" href="/todays-stats">
+        Daily Breakdown
+      </Link>
+    </div>
+  );
+};
+
+export default NavHeader;
