5040d0ef0369203a88d658cf901f789176efd5e8
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-10-21 13:23:42 -0400

    wip: set up service
    adds EarthquakeService, base url, and notes for approach

---
+++ utils/services/Earthquake.ts
@@ -0,0 +1,12 @@
+export class EarthquakeService {
+  private baseURL: string =
+    'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/';
+
+  /** Fetch all earthquakes for the day */
+
+  /** Fetch all earthquakes for the week */
+
+  /** Fetch earthquake stats (graph data) for the day */
+
+  /** Fetch earthquake stats (graph data) for the week */
+}
